f964ccf16700634bed2fcfd003458f76459380ca
mccedddy <cedricmacallan@gmail.com>
2022-04-19 20:22:20 +0800

    Add hobby

--- PythonTest2.py
+++ PythonTest2.py
@@ -1,8 +1,9 @@
 # Input
 firstName = input("First Name: ")
 lastName = input("Last Name: ")
+hobby = input("Hobby: ")
 
 # Print
-print("Hi! My name is", firstName, lastName)
+print("Hi! My name is", firstName, lastName + ".", "My hobby is", hobby + ".")
 
-# Edited from github
+# Added hobby
