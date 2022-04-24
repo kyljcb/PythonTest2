11bb2a9a7a1def07666e913711273c440ccd777e
Kyle Jacob <kylelesterjacob17@gmail.com>
2022-04-24 19:00:13 +0800

    add address

--- PythonTest2.py
+++ PythonTest2.py
@@ -3,9 +3,12 @@ firstName = input("First Name: ")
 lastName = input("Last Name: ")
 age = input("Age: ")
 hobby = input("Hobby: ")
+city = input("City: ")
+province = input("Province: ")
 
 # Print
-print("Hi! My name is", firstName, lastName + ".", "I am", age, "years old.", "My hobby is", hobby + ".")
+print("Hi! My name is", firstName, lastName + ".", "I am", age, "years old.", "My hobby is", hobby + ". I live in", city + ",", province + ".")
 
 # Age done
 # Added hobby
+# Add address
